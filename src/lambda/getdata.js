68940f33884f645cb993f4b5af4044d757558785
khenryjoe <joseph.ketterer@gmail.com>
2019-06-06 17:25:21 +0100

    change lamba header

--- src/lambda/getdata.js
+++ src/lambda/getdata.js
@@ -9,7 +9,8 @@ exports.handler  = function(event, context, callback){
         callback(null, {
             statusCode: 200,
             headers: {
-              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Origin': 'https://opentdb.com/',
+              'Access-Control-Allow-Methods': 'GET',
               'Access-Control-Allow-Headers':
                 'Origin, X-Requested-With, Content-Type, Accept'
             },
