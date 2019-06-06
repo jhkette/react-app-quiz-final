6f786f791a02f311c7cc73292a6c35e8967c05fa
khenryjoe <joseph.ketterer@gmail.com>
2019-06-06 20:48:05 +0100

    css

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,19 @@
 import React from "react";
+import styled, { css } from 'styled-components';
+
+const Quizinfo = styled.div`
+display: flex;
+p{
+  margin-right: 1rem;
+} `
 
 const header = props => {
   return (
-    <div>
+    <Quizinfo>
       <p>Fancy quiz app</p>
       <p>Counter: {props.index + 1}/10</p>
       <p>Score: {props.score}/10</p>
-    </div>
+    </Quizinfo>
   );
  };
 
